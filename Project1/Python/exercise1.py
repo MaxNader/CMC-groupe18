617fb70c99acc01b6a613eb43b6763ae4a13a04e
MaxNader <maxime.chanson@epfl.ch>
2024-04-20 16:24:42 +0200

    heatmap for gridsearch implemented

--- Project1/Python/exercise1.py
+++ Project1/Python/exercise1.py
@@ -1,9 +1,12 @@
 from util.run_closed_loop import run_multiple
 from simulation_parameters import SimulationParameters
 from util.run_closed_loop import run_single
+import plot_results
 import os
 import numpy as np
+import matplotlib.pyplot as plt
 import farms_pylog as pylog
+from util.rw import load_object
 import warnings
 
 warnings.filterwarnings("ignore", category=RuntimeWarning)
@@ -12,13 +15,14 @@ def exercise_multiple():
     log_path = './logs/example_multiple/'
     os.makedirs(log_path, exist_ok=True)
 
-    nsim = 4
+    nsim = 5
     pylog.info(
         "Running multiple simulations in parallel from a list of SimulationParameters")
     all_results = []
     best_fspeed = 0
     best_freq = 0
-    for freq in range(1, 6):
+    freqs = [1]
+    for freq in freqs:
         pars_list = [
             SimulationParameters(
                 simulation_i=i*nsim+j,
@@ -32,13 +36,13 @@ def exercise_multiple():
                 headless=True,
                 print_metrics=False,
                 return_network=True,
-                activation_func = [True,50,0.5]
+                activation_func = [False,50,0.5]
             )
-            for i, amp in enumerate(np.linspace(0.8, 1, nsim))
-            for j, wavefrequency in enumerate(np.linspace(0.6, 0.8, nsim))
+            for i, amp in enumerate(np.linspace(0.01, 2, nsim))
+            for j, wavefrequency in enumerate(np.linspace(0.01, 2, nsim))
         ]
 
-        networks = run_multiple(pars_list, num_process=6)
+        networks = run_multiple(pars_list, num_process=8)
         metrics_dictlist = [net.metrics for net in networks]
 
         sorted_metrics_list = sorted(
@@ -58,6 +62,29 @@ def exercise_multiple():
     for result in all_results:
         freq, best_amp, best_wavefrequency, fspeed = result
         print('For frequency =', freq, 'Hz, best params are: amp =', best_amp, ', wave frequency =', best_wavefrequency, ', fspeed =', fspeed)
+    
+
+    fspeeds = np.zeros([nsim**2, 3])
+
+    for i in range(nsim**2):
+        # Charger le contrôleur depuis le fichier
+        controller = load_object(log_path + "controller" + str(i))
+        
+        # Extraire les valeurs d'amp, wavefrequency et fspeed_PCA
+        fspeeds[i] = [
+            controller.pars.amp,
+            controller.pars.wavefrequency,
+            controller.metrics["fspeed_PCA"]  # Correction de fspeed_cycle à fspeed_PCA
+        ]
+
+
+    plt.figure('exercise_multiple', figsize=[10, 10])
+    plot_results.plot_2d(
+    fspeeds,
+    ['amp', 'wavefrequency', 'Forward Speed [m/s]'],
+    cmap='nipy_spectral'
+    )
+    plt.show()
 
     print("Best frequency with the highest fspeed:", best_freq, "Hz")
     print('RUNNING FASTEST ZEBRAFISH NOW')
