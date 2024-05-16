4f0ce4df784d19f963ae20e47ea66b93dac79342
Ivan-loup <92537124+Ivan-loup@users.noreply.github.com>
2024-05-16 10:31:20 +0200

    exo5

--- Project2/exercise5.py
+++ Project2/exercise5.py
@@ -5,8 +5,13 @@ from util.run_closed_loop import run_multiple
 import numpy as np
 import farms_pylog as pylog
 import os
-
-
+from util.run_closed_loop import run_single
+from simulation_parameters import SimulationParameters
+import matplotlib.pyplot as plt
+import os
+from plotting_common import plot_left_right, plot_trajectory, plot_time_histories, plot_time_histories_multiple_windows
+import farms_pylog as pylog
+from metrics import compute_curvature
 def exercise5():
 
     pylog.info("Ex 5")
@@ -14,7 +19,44 @@ def exercise5():
     log_path = './logs/exercise5/'
     os.makedirs(log_path, exist_ok=True)
 
+    nsim=1
+       
+
+    Idiff_list= [0,1,2,3,4]
+    pars_list = [
+        SimulationParameters(
+            simulation_i=nsim,
+            n_iterations=3001,
+            log_path=log_path,
+            video_record=False,
+            compute_metrics=3,
+            Idiff=Id,
+            return_network=True,
+    )
+    for i, Id in enumerate(Idiff_list)]
+    networks = run_multiple(pars_list, num_process=6)
+    curvatures_list = [net.metrics["curvature"] for net in networks]
+    lat_speed_list = [net.metrics["lspeed_PCA"] for net in networks]
+
+    for i, network in enumerate(networks):
+        curvature=curvatures_list[i]
+        print(f"Idiff = {Idiff_list[i]}")
+        print(f"Curvature: {curvature}\n")
+
+        # Plot trajectory for each Idiff
+        plt.subplot(3, 2, i + 1)
+        plot_trajectory(network)
+        plt.title(f"Trajectory for Idiff = {Idiff_list[i]}")
+
+
+    
+
+        
+
 
 if __name__ == '__main__':
     exercise5()
+    plt.show()
+
+
 
