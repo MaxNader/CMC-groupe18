b5781ff64682df0031bb0175e5f383c3808e71f6
MaxNader <maxime.chanson@epfl.ch>
2024-06-03 19:02:38 +0200

    fig7 and corrected code (+added freq and wavefreq)

--- Project2/exercise7.py
+++ Project2/exercise7.py
@@ -19,7 +19,7 @@ def exercise7():
     
     log_path = './logs/exercise7/'
     os.makedirs(log_path, exist_ok=True)
-    I = 30
+    I = 20
     w_stretch_list= np.linspace(0, 15, 4)
     Idiff_list = np.linspace(0, 4, 4)
     n_sim = 4
@@ -34,9 +34,9 @@ def exercise7():
             stretch = False,
             return_network=True,
             Idiff = Id,
+            n_desc = 8,
             print_metrics=False,
             headless=True,
-
             I=I
     ) for i, gss in enumerate(w_stretch_list) for j, Id in enumerate(Idiff_list)]
     networks = run_multiple(pars_list, num_process=6)
@@ -48,9 +48,12 @@ def exercise7():
     lat_speeds = np.zeros([len(w_stretch_list)* len(Idiff_list),3])
     fspeeds = np.zeros([len(w_stretch_list)* len(Idiff_list),3])
     ptccs = np.zeros([len(w_stretch_list)* len(Idiff_list),3])
+    freqs = np.zeros([len(w_stretch_list)* len(Idiff_list),3])
+    wavefreqs = np.zeros([len(w_stretch_list)* len(Idiff_list),3])
     for i,num in enumerate([i*n_sim+j for i in range(n_sim) for j in range(n_sim)]):
         # load controller
         controller = load_object(log_path+"controller"+str(num))
+
         ptccs[i] = [
             controller.pars.Idiff,
             controller.pars.w_stretch,
@@ -72,32 +75,57 @@ def exercise7():
             np.mean(controller.metrics["curvature"])
         ]
 
+        freqs[i] =  [
+            controller.pars.Idiff,
+            controller.pars.w_stretch,
+            np.mean(controller.metrics["frequency"])
+        ]
+        wavefreqs[i] =  [   
+            controller.pars.Idiff,
+            controller.pars.w_stretch,
+            np.mean(controller.metrics["wavefrequency"])
+        ]
+
     
     plt.figure('curvature{}'.format(I), figsize=[10, 10])
     plot_2d(
         curvatures,
-        ['w_stretch', 'Idiff', 'curvature'],
+        [ 'Idiff','w_stretch', 'curvature'],
         cmap='nipy_spectral'
     )
     plt.figure('lat_speed{}'.format(I), figsize=[10, 10])
     plot_2d(
         lat_speeds,
-        ['w_stretch', 'Idiff', 'lspeed_PCA'],
+        [ 'Idiff', 'w_stretch','lspeed_PCA'],
         cmap='nipy_spectral'
     )
     plt.figure('fspeed{}'.format(I), figsize=[10, 10])
     plot_2d(
         fspeeds,
-        ['w_stretch', 'Idiff', 'fspeed_PCA'],
+        [ 'Idiff', 'w_stretch','fspeed_PCA'],
         cmap='nipy_spectral'
     )
     plt.figure('oscillation stability{}'.format(I), figsize=[10, 10])
     plot_2d(
         ptccs,
-        ['w_stretch', 'Idiff', 'PTCC'],
+        [ 'Idiff', 'w_stretch','PTCC'],
         cmap='nipy_spectral'
     )
-    
+
+
+    plt.figure('frequency{}'.format(I), figsize=[10, 10])
+    plot_2d(
+        freqs,
+        ['Idiff', 'w_stretch', 'frequency'],
+        cmap='nipy_spectral'
+    )
+    plt.figure('wave_frequency{}'.format(I), figsize=[10, 10])
+    plot_2d(
+        wavefreqs,
+        ['Idiff', 'w_stretch', 'wavefrequency'],
+        cmap='nipy_spectral'
+    )
+
     
 
 if __name__ == '__main__':
